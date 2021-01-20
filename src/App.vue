9e6b945037a524ee0f5ebbdbe61148236396bcaa
daniele985@hotmail.it <d.giussani@fagor.local>
2021-01-20 23:36:00 +0100

    Created Header/Footer components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,103 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire"> 
+    <Header></Header>
+    <v-main class="white lighten-3">
+
+      <v-container
+      fluid
+      class="pa-0"
+      >
+        <v-row>
+          <v-col 
+          cols="12"          
+          >
+           <v-img
+           lazy-src="https://picsum.photos/id/11/10/6"    
+           max-height="500"     
+           src="./assets/container2.jpg"
+           > 
+          </v-img>
+          </v-col>      
+        </v-row>
+      </v-container>
+      <v-container
+      fluid
+      color="white"
+      class="pa-0 mb-5"      
+      >
+        <v-row>
+          <v-col
+          cols="12"
+          class="d-flex justify-center flex-column"
+          
+          >
+            <v-row class="ma-0 justify-center">
+              <h1>GIUSSANI DANIELE</h1>
+            </v-row>
+            <v-row class="ma-0 justify-center">
+              <h3>Web Developer</h3>
+            </v-row>
+
+            <v-row class="d-flex ma-5 flex-row justify-center" >
+              <v-btn
+              class="btn ma-5 white--text"
+              color="red"
+              
+              >
+                {{ ciao }}
+              </v-btn>
+              <v-btn 
+              class="btn ma-5 white--text"
+              color="black"
+              >
+                {{ bye }}
+              </v-btn>
+            </v-row>          
+            
+          </v-col>   
+          
+
+        </v-row>
+
+      </v-container>
+    
+    </v-main>  
+    <Footer></Footer>
+    
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  export default {
+
+      data: () => ({      
+      ciao: 'ESPLORA',
+      bye: 'Hello',
+    }),
+      components: {
+      Header, 
+      Footer,           
+    }  
+    
   }
-}
+</script>
+
+<style scoped>
+  h1 {
+    font-size: 70px;
+    font-family:'Courier New', Courier, monospace;
+  }
+  h3 {
+    font-size: 40px;
+    font-family:'Courier New', Courier, monospace;
+  
+  }
+  .btn {
+    font-family: 'Courier New', Courier, monospace;
+  }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>  
+    <v-app-bar
+      app
+      color="red"
+      flat
+    >
+      <v-tabs
+        centered
+        class="ml-n9"
+        color="white"
+      >
+        <v-tab
+          v-for="link in links"
+          :key="link"
+          class="tab"
+        >
+          {{ link }}
+        </v-tab>
+      </v-tabs>
+
+      
+    </v-app-bar>
+</template>
+
+<script>
+export default {
+    
+    data: () => ({
+      links: [
+        'HOME',
+        'CHI SONO',
+        'SERVIZI',
+        'PORTFOLIO',
+        'CONTATTI',
+      ],      
+    }),
+}
+</script>
+
+<style scoped>
+  .tab {
+    font-family:'Courier New', Courier, monospace;
+    font-weight:900;
+    font-size: 16px;
+  }
+  .tab:hover {
+    border-bottom: 2px white solid;
+  }
+
+</style>
